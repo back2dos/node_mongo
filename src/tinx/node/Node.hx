eabb0616f97f07633e9075557acae59acc211d8b
back2dos <back2dos@gmail.com>
2014-04-07 18:07:26 +0200

    Rudimentary stuff working.

---
+++ src/tinx/node/Node.hx
@@ -0,0 +1,39 @@
+package tinx.node;
+
+#if macro
+import haxe.macro.Expr;
+using tink.MacroApi;
+#end
+
+class Node {
+	macro static public function yield(handler, future:Expr)
+		return {
+			var d = future.pos.makeBlankType();
+			var f = future.pos.makeBlankType();
+			macro @:pos(future.pos) {
+				var handler = $handler;
+				($future : tink.core.Future.Surprise<$d, $f>).handle(function (__x) switch __x {
+					case Success(data):
+						handler(null, data);
+					case Failure(error):
+						handler(error, null);
+				});
+			}
+		}
+	macro static public function await(e) 
+		return
+			switch e {
+				case macro $func($a{args}):
+					var tmp = MacroApi.tempName();
+					args.push(macro function (error, result) {
+						if (error != null) $i{tmp}(tink.core.Outcome.Failure(tink.core.Error.withData(error.message, error)));
+						else $i{tmp}(tink.core.Outcome.Success(result));
+					});
+					var ret = macro tink.core.Future.async(function ($tmp) {
+						$func($a{args});
+					});
+					// ret.pos.warning(ret.toString());
+					return ret;
+				default: e.reject();
+			}
+}
---
+++ src/tinx/node/mongo/lang/TypeInfo.hx
@@ -0,0 +1,102 @@
+package tinx.node.mongo.lang;
+
+import haxe.ds.StringMap;
+import haxe.macro.Expr;
+import haxe.macro.Type;
+
+using tink.MacroApi;
+
+class TypeInfo {
+	var fields:StringMap<TypeInfo>;
+	public var t(default, null):Type;
+	public var pos(default, null):Position;
+	var ct:ComplexType;
+	function new(t, pos, ?nonRoot) {
+		this.t = t;
+		this.ct = t.toComplex();
+		this.pos = pos;
+		function reject()
+			pos.error('type $t not supported by MongoDB');
+		
+		this.fields = new StringMap();
+		
+		switch (t.reduce()) {
+			case TAnonymous(_), TMono(_), TDynamic(_):
+				this.fields = t.getFields().map(function (fields:Array<ClassField>) {
+					var ret = new Map();
+					for (f in fields)
+						ret.set(f.name, new TypeInfo(f.type, pos, true));
+					return ret;
+				}).orUse(null);
+			case TAbstract(t, _) if (nonRoot):
+				switch (t.toString()) {
+					case 'Int', 'Float', 'Bool', 'tinx.node.mongo.ObjectID':
+					default: 
+						if (t.get().meta.has(':equiv')) {}
+						else reject();
+				}
+
+			case TInst(t, params):
+				if (nonRoot)
+					switch (t.toString()) {
+						case 'Date', 'String':
+						case 'Array': fields.set('[]', new TypeInfo(params[0], pos, true));
+						default: reject();
+					}
+				else 
+					switch t.get().kind {
+						case KTypeParameter(constraints):
+							for (c in constraints)
+								for (f in pos.getOutcome(c.getFields())) 
+									this.fields.set(f.name, new TypeInfo(f.type, pos, true));
+						default:
+					}
+			case other: 
+				if (nonRoot) 
+					reject();
+				else 
+					pos.error('type $other not supported for collections');
+		}
+	}
+	public function getFields()
+		return fields.keys();
+		
+	public function has(name) 
+		return fields == null || fields.exists(name);
+	
+	public function isArray()
+		return fields.exists('[]');
+	
+	public function blank(?pos) 
+		return ECheckType(macro null, ct).at(pos == null ? this.pos : pos);
+	
+	public function get(name, pos:Position) 
+		return
+			if (fields == null)
+				new TypeInfo((macro null).typeof().sure(), pos, true)
+			else if (fields.exists(name)) 
+				fields.get(name)
+			else 
+				pos.error('unknown field $name');
+		
+	public function resolve(path:Path) {
+		var ret = this;
+		for (p in path)
+			ret = ret.get(p.s, p.pos);
+		return ret;
+	}
+	public function check(path:Path, value:Null<Expr>) {
+		if (value == null) 
+			throw 'NI';
+		else 
+			ECheckType(value, resolve(path).blank(path.getPos(this.pos)).typeof().sure().toComplex()).at(path.getPos(this.pos)).typeof().sure();
+	}
+	static public function getInfo(e:Expr) {
+		return 
+			switch (e.typeof().sure().reduce()) {
+				case TInst(_, params): new TypeInfo(params[0], e.pos);
+				case TMono(_): new TypeInfo((macro { } ).typeof().sure(), e.pos);
+				default: throw (e.typeof());
+			}
+	}	
+}
